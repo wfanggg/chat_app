635971ae33f8d655c1df578bb03fcc8259b0b172
wfanggg <wfang032@gmail.com>
2022-12-18 13:22:48 +0800

    select user

--- app/src/main/java/com/example/chatapp/firebase/activities/MainActivity.java
+++ app/src/main/java/com/example/chatapp/firebase/activities/MainActivity.java
@@ -37,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void setListeners() {
         binding.imageSignOut.setOnClickListener(v -> signOut());
+        binding.fabNewChat.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), UsersActivity.class)));
     }
 
     private void loadUserDetails(){
@@ -59,7 +61,6 @@ public class MainActivity extends AppCompatActivity {
         DocumentReference documentReference =
                 database.collection(Constants.KEY_COLLECTION_USERS).document(preferenceManager.getString(Constants.KEY_USER_ID));
         documentReference.update(Constants.KEY_FCM_TOKEN, token)
-                .addOnSuccessListener(unused -> showToast("Token updated successfully"))
                 .addOnFailureListener(e -> showToast("Unable to update token"));
     }
 
